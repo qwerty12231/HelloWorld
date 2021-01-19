2411514e1e1a443cac5fad9ff937fd24d78ee77a
qwerty12231 <77588490+qwerty12231@users.noreply.github.com>
2021-01-19 16:35:37 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,19 @@
+name = input("Enter your name: ")
+print("Hi: ", name, ".")
+
+or_password = ("5454545")
+password = input("Enter your password: ")
+
+if password == or_password:
+    print("OK!")
+else:
+    print("Error.")
+
+age = input("How old are you: ?")
+print("_")
+
+if age >= ("18"):
+    print("OK")
+if age < ("18"):
+    print("Error.")
+
