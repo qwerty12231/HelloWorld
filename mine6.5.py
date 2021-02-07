b48c8c9fc7823646976898ccd456a474c0c1d9f8
qwerty12231 <77588490+qwerty12231@users.noreply.github.com>
2021-02-07 21:12:40 +0300

    Add files via upload

---
+++ mine6.5.py
@@ -0,0 +1,36 @@
+"""Реализовать класс Stationery. Определить в нем атрибут title и метод draw.
+Метод выводит сообщение “Запуск отрисовки.” Создать три дочерних класса Pen, Pencil, Handle.
+В каждом из классов реализовать переопределение метода draw.
+Для каждого из классов метод должен выводить уникальное сообщение.
+Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра."""
+
+
+class Stationery:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        return f'Запуск отрисовки'
+
+
+class Pen(Stationery):
+    def draw(self):
+        return f'Запуск отрисовки {self.title}'
+
+
+class Pencil(Stationery):
+    def draw(self):
+        return f'Запуск отрисовки {self.title}'
+
+
+class Handle(Stationery):
+    def draw(self):
+        return f'Запуск отрисовки {self.title}'
+
+
+pen = Pen('ручкой')
+print(pen.draw())
+pencil = Pencil('карандашом')
+print(pencil.draw())
+handle = Handle('маркером')
+print(handle.draw())
